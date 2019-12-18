eb13332402587a5c6e8c35c95a04b871daf933e2
Yurika Jachmann <yj647@nyu.edu>
2019-12-17 22:42:09 -0500

    Update file read of secret

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = open("/run/secrets/csrf_token", "r").rstrip()
+    with open('/run/secrets/csrf_token', 'r') as f:
+        SECRET_KEY = f.read().strip()
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
